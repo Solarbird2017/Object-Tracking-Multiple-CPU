1eade22ed3aa210a92dc8bb14f4a401d291cae64
Solarbird2017 <nickzusa@gmail.com>
2020-11-13 23:59:15 -0500

    Add files via upload

---
+++ hpc_mini/logFile.h
@@ -0,0 +1,37 @@
+//
+// Created by Nick Zhang on 2019-03-30.
+//
+
+#ifndef HPC_ADAPTIVE_LOGFILE_H
+#define HPC_ADAPTIVE_LOGFILE_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using std::string;
+using std::fstream;
+
+void writeResultsToLog(string timecom, string Nsteps, string NPUs,
+        string NPs, string rmse, string NPstrans){
+    string logFile = "log.txt";
+    fstream filestr;
+    filestr.open (logFile.c_str(), fstream::in | fstream::out | fstream::trunc);
+
+    filestr << "Results of Minimal Transfer Resampling: " << endl;
+
+    filestr << "timeComsumption (Second): " << timecom << endl;
+    filestr << "Number of Time Steps: " << Nsteps << endl;
+    filestr << "Number of PU: " << NPUs << endl;
+    filestr << "Number of Particles: " << NPs << endl;
+    filestr << "RMSE: " << rmse << endl;
+
+//    filestr << "Neff Threshold: " << NeffThre << endl;
+    filestr << "NofparTrans (Global): " << NPstrans<< endl;
+//    filestr << "Number of Par trans between PU (Local): " << NPstransLoc << endl;
+//    filestr << "TimeInterval: " << TimeInterval << endl;
+    filestr.close();
+}
+#endif //HPC_ADAPTIVE_LOGFILE_H
+
